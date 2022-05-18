9ba27a97e92bbe18f3e33289d35e5da284447158
Chaitali D <cudandale@gmail.com>
2022-05-17 20:08:01 -0400

    fix: bugfix-002-store-notes-localstorage-function - Fixed function to store the notes the localstorage to avoid data loss on page reload

--- src/App.js
+++ src/App.js
@@ -2,11 +2,10 @@
 import './App.css';
 import NoteContainer from './Components/NoteContainer/NoteContainer';
 import Sidebar from './Components/Sidebar/Sidebar';
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
 
 function App() {
-  const [notes,setNotes] =useState([
-  ]);
+  const [notes,setNotes] =useState(JSON.parse(localStorage.getItem('notes-app')) || []);
 
   const addNote = (color) =>{
     const tempNotes =[...notes];
@@ -26,10 +25,23 @@ function App() {
     let newNotes = tempNotes.filter(element => element.id !== id);
     setNotes(newNotes);
   }
+
+  useEffect(() => {
+    localStorage.setItem('notes-app',JSON.stringify(notes))
+  })
+
+  const updateText = (text, id) =>{
+    const tempNotes =[...notes];
+    const index= tempNotes.findIndex((item) => item.id === id);
+    if(index < 0) return ;
+    tempNotes[index].text = text;
+    setNotes(tempNotes);
+  }
+
   return (
     <div className="App">  
       <Sidebar addNote={addNote}/>
-      <NoteContainer notes={notes} deleteNote={deleteNote}/>
+      <NoteContainer notes={notes} deleteNote={deleteNote} updateText={updateText}/>
     </div>
   );
 }
